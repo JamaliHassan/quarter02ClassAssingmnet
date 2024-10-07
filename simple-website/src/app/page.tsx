eb98593c6305b7032b59be2436188299a14942ad
JamaliHassan <balochhassan939@gmail.com>
2024-10-07 22:14:19 +0500

    bug fix

--- simple-website/src/app/page.tsx
+++ simple-website/src/app/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 import { Fragment } from "react";
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <Fragment>
       <Navbar />
-        <div className=" flex justify-center items-center h-[200px] w-[400px] p-10 mt-10 m-auto  text-xl bg-gradient-to-r from-cyan-500 to-blue-500">
-          Home Page Content
-        </div>
+      <div className=" flex justify-center items-center h-[200px] w-[400px] p-10 mt-10 m-auto  text-xl bg-gradient-to-r from-cyan-500 to-blue-500">
+        Home Page Content
+      </div>
     </Fragment>
   );
 }
